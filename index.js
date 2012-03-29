9265118087ab365c0d6f48e6bc7051fc54ba294f
Moto Ishizawa <summerwind.jp@gmail.com>
2012-03-29 23:24:58 +0900

    core: Use apply() with arguments

--- index.js
+++ index.js
@@ -100,225 +100,226 @@ var restrictor = {
 fs.rename = function(path1, path2, callback) {
     restrictor.checkPath(path1);
     restrictor.checkPath(path2);
-    return rename.call(fs, path1, path2, callback);
+    return rename.apply(fs, arguments);
 };
 
 fs.renameSync = function(path1, path2) {
     restrictor.checkPath(path1);
     restrictor.checkPath(path2);
-    return renameSync.call(fs, path1, path2);
+    return renameSync.apply(fs, arguments);
 };
 
 fs.chown = function(path, uid, gid, callback) {
     restrictor.checkPath(path);
-    return chown.call(fs, path, uid, gid, callback);
+    return chown.apply(fs, arguments);
 };
 
 fs.chownSync = function(path, uid, gid) {
     restrictor.checkPath(path);
-    return chownSync.call(fs, path, uid, gid);
+    return chownSync.apply(fs, arguments);
 };
 
 fs.lchown = function(path, uid, gid, callback) {
     restrictor.checkPath(path);
-    return lchown.call(fs, path, uid, gid, callback);
+    return lchown.apply(fs, arguments);
 };
 
 fs.lchownSync = function(path, uid, gid) {
     restrictor.checkPath(path);
-    return lchownSync.call(fs, path, uid, gid);
+    return lchownSync.apply(fs, arguments);
 };
 
 fs.chmod = function(path, mode, callback) {
     restrictor.checkPath(path);
-    return chmod.call(fs, path, mode, callback);
+    return chmod.apply(fs, arguments);
 };
 
 fs.chmodSync = function(path, mode) {
     restrictor.checkPath(path);
-    return chmodSync.call(fs, path, mode);
+    return chmodSync.apply(fs, arguments);
 };
 
 fs.lchmod = function(path, mode, callback) {
     restrictor.checkPath(path);
-    return lchmod.call(fs, path, mode, callback);
+    return lchmod.apply(fs, arguments);
 };
 
 fs.lchmodSync = function(path, mode) {
     restrictor.checkPath(path);
-    return lchmodSync.call(fs, path, mode);
+    return lchmodSync.apply(fs, arguments);
 };
 
 fs.stat = function(path, callback) {
     restrictor.checkPath(path);
-    return stat.call(fs, path, callback);
+    return stat.apply(fs, arguments);
 };
 
 fs.statSync = function(path) {
     restrictor.checkPath(path);
-    return statSync.call(fs, path);
+    return statSync.apply(fs, arguments);
 };
 
 fs.lstat = function(path, callback) {
     restrictor.checkPath(path);
-    return lstat.call(fs, path, callback);
+    return lstat.apply(fs, arguments);
 };
 
 fs.lstatSync = function(path) {
     restrictor.checkPath(path);
-    return lstatSync.call(fs, path);
+    return lstatSync.apply(fs, arguments);
 };
 
 fs.lstat = function(path, callback) {
     restrictor.checkPath(path);
-    return lstat.call(fs, path, callback);
+    return lstat.apply(fs, arguments);
 };
 
 fs.lstatSync = function(path) {
     restrictor.checkPath(path);
-    return lstatSync.call(fs, path);
+    return lstatSync.apply(fs, arguments);
 };
 
 fs.link = function(srcpath, dstpath, callback) {
     restrictor.checkPath(srcpath);
     restrictor.checkPath(dstpath);
-    return link.call(fs, srcpath, dstpath, callback);
+    return link.apply(fs, arguments);
 };
 
 fs.linkSync = function(srcpath, dstpath) {
     restrictor.checkPath(srcpath);
     restrictor.checkPath(dstpath);
-    return linkSync.call(fs, srcpath, dstpath);
+    return linkSync.apply(fs, arguments);
 };
 
 fs.symlink = function(linkdata, path, type, callback) {
     restrictor.checkPath(linkdata);
     restrictor.checkPath(path);
-    return symlink.call(fs, linkdata, path, type, callback);
+    return symlink.apply(fs, arguments);
 };
 
 fs.symlinkSync = function(linkdata, path, type) {
     restrictor.checkPath(linkdata);
     restrictor.checkPath(path);
-    return symlinkSync.call(fs, linkdata, path, type);
+    return symlinkSync.apply(fs, arguments);
 };
 
 fs.readlink = function(path, callback) {
     restrictor.checkPath(path);
-    return readlink.call(fs, path, callback);
+    return readlink.apply(fs, arguments);
 };
 
 fs.readlinkSync = function(path) {
     restrictor.checkPath(path);
-    return readlinkSync.call(fs, path);
+    return readlinkSync.apply(fs, arguments);
 };
 
 fs.unlink = function(path, callback) {
     restrictor.checkPath(path);
-    return unlink.call(fs, path, callback);
+    return unlink.apply(fs, arguments);
 };
 
 fs.unlinkSync = function(path) {
     restrictor.checkPath(path);
-    return unlinkSync.call(fs, path);
+    return unlinkSync.apply(fs, arguments);
 };
 
 fs.rmdir = function(path, callback) {
     restrictor.checkPath(path);
-    return rmdir.call(fs, path, callback);
+    return rmdir.apply(fs, arguments);
 };
 
 fs.rmdirSync = function(path) {
     restrictor.checkPath(path);
-    return rmdirSync.call(fs, path);
+    return rmdirSync.apply(fs, arguments);
 };
 
 fs.mkdir = function(path, mode, callback) {
     restrictor.checkPath(path);
-    return mkdir.call(fs, path, mode, callback);
+    return mkdir.apply(fs, arguments);
 };
 
 fs.mkdirSync = function(path, mode) {
     restrictor.checkPath(path);
-    return mkdirSync.call(fs, path, mode);
+    return mkdirSync.apply(fs, arguments);
 };
 
 fs.readdir = function(path, callback) {
     restrictor.checkPath(path);
-    return readdir.call(fs, path, callback);
+    return readdir.apply(fs, arguments);
 };
 
 fs.readdirSync = function(path) {
     restrictor.checkPath(path);
-    return readdirSync.call(fs, path);
+    return readdirSync.apply(fs, arguments);
 };
 
 fs.open = function(path, flags, mode, callback) {
     restrictor.checkPath(path);
-    return open.call(fs, path, flags, mode, callback);
+    return open.apply(fs, arguments);
 };
 
 fs.openSync = function(path, flags, mode) {
     restrictor.checkPath(path);
-    return openSync.call(fs, path, flags, mode);
+    return openSync.apply(fs, arguments);
 };
 
 fs.utimes = function(path, atime, mtime, callback) {
     restrictor.checkPath(path);
-    return utimes.call(fs, path, atime, mtime);
+    return utimes.apply(fs, arguments);
 };
 
 fs.utimesSync = function(path, atime, mtime) {
     restrictor.checkPath(path);
-    return utimesSync.call(fs, path, atime, mtime);
+    return utimesSync.apply(fs, arguments);
 };
 
 fs.readFile = function(filename, encoding, callback) {
     restrictor.checkPath(filename);
-    return readFile.call(fs, filename, encoding, callback);
+    return readFile.apply(fs, arguments);
 };
 
 fs.readFileSync = function(filename, encoding) {
     restrictor.checkPath(filename);
-    return readFileSync.call(fs, filename, encoding);
+    return readFileSync.apply(fs, arguments);
 };
 
 fs.writeFile = function(filename, data, encoding, callback) {
     restrictor.checkPath(filename);
-    return writeFile.call(fs, filename, data, encoding, callback);
+    return writeFile.apply(fs, arguments);
 };
 
 fs.writeFileSync = function(filename, data, encoding) {
     restrictor.checkPath(filename);
-    return writeFileSync.call(fs, filename, data, encoding);
+    return writeFileSync.apply(fs, arguments);
 };
 
 fs.watchFile = function(filename, options, listener) {
     restrictor.checkPath(filename);
-    return watchFile.call(fs, filename, options, listener);
+    return watchFile.apply(fs, arguments);
 };
 
 fs.watch = function(filename, options, listener) {
     restrictor.checkPath(filename);
-    return watch.call(fs, filename, options, listener);
+    return watch.apply(fs, arguments);
 };
 
 fs.createReadStream = function(path, options) {
     restrictor.checkPath(path);
-    return createReadStream.call(fs, path, options);
+    return createReadStream.apply(fs, arguments);
 };
 
 fs.createWriteStream = function(path, options) {
     restrictor.checkPath(path);
-    return createWriteStream.call(fs, path, options);
+    return createWriteStream.apply(fs, arguments);
 };
 
 path.exists = function(p, callback) {
     restrictor.checkPath(p);
-    return exists.call(path, p, callback);
+    return exists.apply(path, arguments);
 };
 
 path.existsSync = function(p) {
     restrictor.checkPath(p);
-    return existsSync.call(path, p);
+    return existsSync.apply(path, arguments);
 };
+
